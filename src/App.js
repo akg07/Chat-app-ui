cc8809d581c9a0acbfc67d7e9564a338962dacba
Ayush <ayush3032@gmail.com>
2024-07-05 00:01:12 +0530

    create chat-context.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,15 @@ import "./App.css";
 import HomePage from "./pages/HomePage";
 import ChatPage from "./pages/ChatPage";
 
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route exact path="/" element={<HomePage />} />
-          <Route path="/chats" element={<ChatPage />} />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route path="/chats" element={<ChatPage />} />
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
+import ChatProvider from "./context/ChatProvider";
+import { BrowserRouter as Router } from "react-router-dom";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>
+  <Router>
+    <ChatProvider>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </ChatProvider>
+  </Router>
 );
